f3dafa9bcb03000d4e74761b4e076bbf19dcef91
AunAun <passawut.a.plus@gmail.com>
2026-05-31 15:36:15 +0700

    แก้ config กลับเป็น Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,4 @@
 // You can pass additional config via defineConfig({ vite: { ... } }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-export default defineConfig({
-  server: {
-    preset: "vercel"
-  }
-});
+export default defineConfig();
